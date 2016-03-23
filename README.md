0600db377e356677d82d8354852d3a5215affe5b
unknown <MY324297@L-156026719.wipro.com>
2016-03-23 14:32:41 +0530

    read me

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GitAnalyzer
+
+This application lets you enter any Git Repo URL and observe metrics like No of Open Issues, etc.,
+
+1. To run the application clone the repository from [GitAnalyzer](https://github.com/uday4393/GitAnalyzer.git)
+2. Deploy app using any environment as I just implemented the functionality using ajax and Jquery.
+3. After Deploying in your Dev environment, Run the app.
+
+###License
+This project is licensed under the terms of the Apache-2.0 license
--- public/js/gitService.js
+++ public/js/gitService.js
@@ -1,3 +1,6 @@
+/*This code is written by Uday Kumar Mydam*/
+
+//Simple JQuery function
 $(function(){
   $('#submitURL').on('click', function(e){
     e.preventDefault();
@@ -5,34 +8,37 @@ $(function(){
     var requri   = $('#GitInput_form').val().split('/');
     var repouri  = 'https://api.github.com/repos/'+requri[3]+'/'+requri[4]+'/issues?per_page=100';
 
+//repouri is the way to access the Anything related to issues
     requestJSON(repouri, function(json) {
       if(json.message == "Not Found" || $('#GitInput_form').val() == '') {
         $('#table').html("<h2>No data Found</h2>");
       }
 
       else {
-        // else we have a user and we display their info
+        // else openissues is directly the length of the Json Array of Objects
         var openissues = json.length;
         var issuesInDay = 0;
         var issues7Ddays = 0;
         var moreThan7days = 0;
 
+        //update the total issue count
         $('#item1').html(openissues);
         var currentdate = new Date();
 
         //for each object calculate date
         $.each(json, function(i){
-        //  console.log(json[i].created_at);
           var dt = new Date(json[i].created_at);
             var t1=dt.getTime();
             var t2=currentdate.getTime();
+            //calculate differnce in days
             var days = parseInt((t2-t1)/(24*3600*1000));
 
-            if(days <= 1){issuesInDay++;}
-            if(days > 1 && days <= 7){issues7Ddays++;}
-            if(days >= 7){moreThan7days++;}
+            if(days < 1){issuesInDay++;}
+            if(days >= 1 && days <= 7){issues7Ddays++;}
+            if(days > 7){moreThan7days++;}
         });
 
+        //update DOM elements
         $('#item2').html(issuesInDay);
         $('#item3').html(issues7Ddays);
         $('#item4').html(moreThan7days);
